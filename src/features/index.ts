4d33b791897e0b2af710644aea0758bb13598d23
Kamiel Wanrooij <kamielw@amazon.com>
2023-08-31 09:47:25 +0200

    Provide `Server` interface for providers to implement their language servers
    This change provides the `Server` interface through which language server providers
    can host their language server in one or more runtimes. The `Server` interface
    exposes a numbe of features, including `LSP` and `Auth`, to be used in language
    servers. This allows a portable, reusable interface for common operations and
    interactions that can be hosted in a number of different runtimes, tools, and
    environments.
    
    The `standalone` runtime is provided as an initial and incomplete implementation.
    It will be expanded on as future features become available. Likewise, some features
    like LSP and Telemetry will be expand in future revisions.

---
+++ src/features/index.ts
@@ -0,0 +1,6 @@
+export { Auth } from "./auth"
+export { Logging } from "./logging"
+export { Lsp } from "./lsp"
+export { Telemetry } from "./telemetry"
+export { Workspace } from "./workspace"
+
---
+++ src/features/telemetry.ts
@@ -0,0 +1,16 @@
+// TODO: implement the richer Metric type to support more complex values and dimensions
+export type Metric = {
+    name: string,
+    value: number,
+}
+
+export const metric = (name: string, value: number): Metric => ({ name, value })
+
+/**
+ * The telemetry feature interface.
+ * 
+ * TODO: define the {Metric} type.
+ */
+export type Telemetry = {
+    emit: (metric: Metric) => void
+}
