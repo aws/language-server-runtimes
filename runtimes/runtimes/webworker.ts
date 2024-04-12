c5d6b3f64f776cb86701d341817394e0916df26d
Cristian Rotari <rtarcr@amazon.com>
2024-04-12 16:13:27 +0000

    Enable injection of credentials provider

--- runtimes/runtimes/webworker.ts
+++ runtimes/runtimes/webworker.ts
@@ -96,6 +96,7 @@ export const webworker = (props: RuntimeProps) => {
         onFollowUpClicked: handler => lspConnection.onNotification(followUpClickNotificationType.method, handler),
     }
 
+    
     let credentialsProvider: CredentialsProvider
     if (props.credentialsProvider) {
         credentialsProvider = props.credentialsProvider
