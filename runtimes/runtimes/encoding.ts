7393833be786e5b9d602a4ae0ade7c43895435bd
Oksana Volodkevych <oksanaov@amazon.com>
2024-11-20 13:50:36 +0100

    Encoding improvements

--- runtimes/runtimes/encoding.ts
+++ runtimes/runtimes/encoding.ts
@@ -3,30 +3,26 @@ export interface Encoding {
     encode(value: string): string
 }
 
+const HEX_PAD: string = '00'
+const HEX_REGEX: RegExp = /%([0-9A-F]{2})/g
 export class WebBase64Encoding implements Encoding {
     constructor(private window: WindowOrWorkerGlobalScope) {}
 
     decode(value: string): string {
-        if (!value) {
-            return value
-        }
         const decoded = this.window.atob(value)
         // to support Unicode chars
         return decodeURIComponent(
             Array.from(decoded)
                 .map(char => {
-                    return '%' + ('00' + char.charCodeAt(0).toString(16)).slice(-2)
+                    return '%' + (HEX_PAD + char.charCodeAt(0).toString(16)).slice(-2)
                 })
                 .join('')
         )
     }
 
     encode(value: string): string {
-        if (!value) {
-            return value
-        }
         // to support Unicode chars
-        const converted = encodeURIComponent(value).replace(/%([0-9A-F]{2})/g, (_, arg) => {
+        const converted = encodeURIComponent(value).replace(HEX_REGEX, (_, arg) => {
             return String.fromCharCode(parseInt(arg, 16))
         })
         return this.window.btoa(converted)
