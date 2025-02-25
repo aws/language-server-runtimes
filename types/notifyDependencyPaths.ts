a5ed0158bda03f987e8a8edd5efd808b080bd296
Ege Ozcan <eoozca@amazon.com>
2025-02-25 17:46:08 +0100

    chore: add docstrings to notifyDependencyPathsParams

--- types/notifyDependencyPaths.ts
+++ types/notifyDependencyPaths.ts
@@ -1,10 +1,27 @@
 export const NOTIFY_DEPENDENCY_PATHS_NOTIFICATION_METHOD = 'aws/notifyDependencyPaths'
 
+/**
+ * Parameters for notifying AWS language server about dependency paths
+ */
 export interface NotifyDependencyPathsParams {
+    /** Name of the module being processed */
     moduleName: string
-    programmingLangugage: string
+    /** Programming language runtime (e.g., 'javascript', 'python', 'java') */
+    runtimeLanguage: string
+    /** Absolute paths to dependency files*/
     files: string[]
+    /** Absolute paths to dependency directories*/
     dirs: string[]
+    /**
+     * Glob patterns to include specific files/directories
+     * Patterns should conform to https://github.com/isaacs/node-glob
+     * @optional
+     */
     includePatterns?: string[]
+    /**
+     * Glob patterns to exclude specific files/directories
+     * Patterns should conform to https://github.com/isaacs/node-glob
+     * @optional
+     */
     excludePatterns?: string[]
 }
