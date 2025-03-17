b7157ab51556bd3d777b11692a4f8563228d2c7e
Patrick van Beurden <202625433+patrickvbeurden@users.noreply.github.com>
2025-03-17 12:08:36 +0100

    feat(runtimes): extend awsClientCapabilities generically to signal server-specific capabilities

--- runtimes/protocol/lsp.ts
+++ runtimes/protocol/lsp.ts
@@ -107,6 +107,7 @@ export interface AWSInitializationOptions {
      * The client signals AWS capabilities it supports.
      */
     awsClientCapabilities?: {
+        [key: string]: any
         window?: {
             notifications?: boolean
         }
