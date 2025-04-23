5a33829e70324afed637e3542ec09b2e4c59581d
Tai Lai <tlaiamz@amazon.com>
2025-04-22 23:30:06 -0700

    feat: allow write stream for tool invocations

--- runtimes/server-interface/agent.ts
+++ runtimes/server-interface/agent.ts
@@ -123,7 +123,7 @@ export type Agent = {
      */
     addTool: <T extends InferSchema<S['inputSchema']>, S extends ToolSpec, R>(
         spec: S,
-        handler: (input: T, token?: CancellationToken) => Promise<R>
+        handler: (input: T, token?: CancellationToken, updates?: WritableStream) => Promise<R>
     ) => void
 
     /**
@@ -136,7 +136,7 @@ export type Agent = {
      * @param input The input to the tool
      * @returns The result of the tool execution
      */
-    runTool: (toolName: string, input: any, token?: CancellationToken) => Promise<any>
+    runTool: (toolName: string, input: any, token?: CancellationToken, updates?: WritableStream) => Promise<any>
 
     /**
      * Get the list of tools in the local tool repository.
