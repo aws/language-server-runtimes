d0250699ff9a82a49b1a0c2d56606be0249b0101
Ramon Li <liramon@amazon.com>
2025-08-01 12:29:59 -0400

    Revert "feat: add optional encryption to updateProfile"
    This reverts commit c0f83ffcc4b49d87cc0990fee3f9cb82ee11145d.

--- runtimes/runtimes/standalone.ts
+++ runtimes/runtimes/standalone.ts
@@ -35,11 +35,9 @@ import {
     stsCredentialChangedRequestType,
     getMfaCodeRequestType,
     profileChangedRequestType,
-    UpdateProfileParams,
 } from '../protocol'
 import { ProposedFeatures, createConnection } from 'vscode-languageserver/node'
 import {
-    decryptObjectWithKey,
     encryptIamResultWithKey,
     EncryptionInitialization,
     encryptObjectWithKey,
@@ -307,15 +305,7 @@ export const standalone = (props: RuntimeProps) => {
 
         const identityManagement: IdentityManagement = {
             onListProfiles: handler => lspConnection.onRequest(listProfilesRequestType, handler),
-            onUpdateProfile: handler =>
-                lspConnection.onRequest(
-                    updateProfileRequestType,
-                    async (params: UpdateProfileParams | string, token: CancellationToken) => {
-                        const decrypted: UpdateProfileParams =
-                            typeof params === 'string' ? await decryptObjectWithKey(params, encryptionKey!) : params
-                        return await handler(decrypted, token)
-                    }
-                ),
+            onUpdateProfile: handler => lspConnection.onRequest(updateProfileRequestType, handler),
             onGetSsoToken: handler =>
                 lspConnection.onRequest(
                     getSsoTokenRequestType,
